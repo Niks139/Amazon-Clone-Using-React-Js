a7d69f28a737b5c6c074b8c52f448193115a913c
Nirmit Patel <55250776+Niks139@users.noreply.github.com>
2021-04-10 23:25:52 -0400

    Minor changes

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import '../css/footer.css'
 
-
+import Flag from '../img/canada.png'
 const Footer = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg fixed-bottom navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg fixed-bottom navbar-dark bg-dark align-items-center">
 
-                <div className="d-flex justify-content-evenly">
-                    <a href="https://www.facebook.com/Amazon">
+                <div className="d-flex align-items-center justify-content-center">
+                    <span className="span__color mx-5">
+                        Privacy Notice
+                    </span>
+                    <a href="https://www.facebook.com/Amazon" className="mx-5">
                         <span>
-                        <i class="fab fa-facebook"></i>
+                        <i className="fab fa-facebook"></i>
                         </span>
                     </a>
-                    <span className="span__color">
+                    <span className="span__color mx-5">
                     © 2008-2021, Amazon.com, Inc. or its affiliates
                     </span>
-                    
-                    
+                    <span className="span__color mx-5">
+                    <img src={Flag} alt="Canada Flag" className="flag" width="10px" />
+                    &nbsp;Canada
+                    </span>
                 </div>
             </nav>
         </div>
